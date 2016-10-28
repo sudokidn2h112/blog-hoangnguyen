6774827cf4ea8c034330c0f6256ce32c8a22b013
sudokid <dkny.sudokidn2h1112@gmail.com>
2016-10-28 15:24:47 +0700

    change video

--- index.js
+++ index.js
@@ -12,27 +12,17 @@ app.set("views","./views");
 var port = process.env.PORT || 6969;
 
 var mang = [
-  new VideoVimeo(1, "187766126_th.jpg", "187766126", "Như Phút Ban Đầu - Noo Phước Thịnh", "Vì em anh như người điên mất trí. Vì em anh như chẳng còn biết nghĩ suy. Vì anh đã trót lỡ đắm say em không bận tâm mai sau thế nào..."),
-  new VideoVimeo(2, "187759316_th.jpg", "187759316", "Chúng ta không thuộc về nhau - Sơn Tùng MTP", "Niềm tin đã mất, giọt nước mắt cuốn kí ức anh chìm sâu. Tìm về nơi đâu, cô đơn đôi chân lạc trôi giữa bầu trời..."),
-  new VideoVimeo(3, "187781709_th.jpg", "187781709", "Gửi Anh Xa Nhớ - Bích Phương", "Anh xa nhớ anh có khỏe không. Em lâu lắm không viết thư tay..."),
-  new VideoVimeo(4, "187780178_th.jpg", "187780178", "We Don't Talk Anymore-Charlie-Puth vs Selena Gomez", "We Don't Talk Anymore, We Don't Talk Anymore..."),
-  new VideoVimeo(5, "187795671_th.jpg", "187795671", "Talk Love - K.Will", " Descendants Of The Sun OST "),
-  new VideoVimeo(6, "188116372_th.jpg", "188116372", "Always - Yoon Mi Rae ", " Descendants Of The Sun OST "),
-  new VideoVimeo(7, "188116347_th.jpg", "188116347", "Everytime - EXO’s Chen & Punch", " Descendants Of The Sun OST"),
-  new VideoVimeo(8, "188116375_th.jpg", "188116375", "You are my everything - Gumm", " Descendants Of The Sun OST"),
-  new VideoVimeo(9, "188116374_th.jpg", "188116374", "This Love - Davichi", " Descendants Of The Sun OST"),
-  new VideoVimeo(10, "188116373_th.jpg", "188116373", "I Do - 911 ", "My whole world changed from the moment I met you. And it would never be the same..."),
-  new VideoVimeo(11, "188236513_th.jpg", "188236513", "Really Love You - Noo Phước Thịnh", " Dịu dàng ngày em đến. Nồng nàn yêu thương. Xua tan cô đơn trong lòng anh..."),
-  new VideoVimeo(12, "188116625_th.jpg", "188116625", "Mãi Mãi Bên Nhau - Noo Phước Thịnh", " Em là ai giữa cuộc đời này. Em từ đâu bước tới nơi đây..."),
-  new VideoVimeo(13, "188274119_th.jpg", "188274119", "Once Again - Kim Na Young & Mad Clown", "  Descendants Of The Sun OST"),
-  new VideoVimeo(14, "188596314_th.jpg", "188596314", "You Were Born To Be Loved", " Taengoo ah"),
-  new VideoVimeo(15, "188776618_th.jpg", "188776618", "Mashup-12 bài hot", " Lynkee"),
-  new VideoVimeo(15, "189267563_th.jpg", "189267563", "Cause I Love You - Noo Phước Thịnh ", "Giờ người đang bước bên ai không phải anh. Giờ người đang bên ai kia không phải anh..."),
-  new VideoVimeo(15, "189267301_th.jpg", "189267301", "Thương - Karik ft Uyên Pím", "Lạ, yêu, hi sinh rồi thương. Tình cảm luôn dẫn dắt ta đến những cảm xúc vô thường"),
-  new VideoVimeo(15, "189267241_th.jpg", "189267241", "Đếm Ngày Xa Em - Only C ft. Lou Hoàng", "Nào đâu, chẳng được bấy lâu. Rồi phải xa cách nhau...."),
-  new VideoVimeo(15, "189267234_th.jpg", "189267234", "ERIK from ST.319 - Sau Tất Cả", "Sau tất cả, mình lại trở về với nhau. Tựa như chưa bắt đầu, tựa như ta vừa mới quen..."),
-  new VideoVimeo(15, "189267207_th.jpg", "189267207", "Yêu Một Người Có Lẽ - Lou Hoàng - Miu Lê ", "Giả vờ như tất cả mọi thứ chưa xảy ra với anh. Lập lờ những thứ ngay trước mắt anh chẳng hề so sánh..."),
-  new VideoVimeo(15, "189267206_th.jpg", "189267206", "Giá Như Anh Lặng Im - OnlyC ft. Lou Hoàng ft. Quang Hùng ", "Vì sao anh cứ đắm chìm lệ hoen cay khóe mi gió đông sang lạnh buốt con tim. Những ngọt ngào xưa nay cũng chỉ là kỉ niệm GIÁ NHƯ ANH LẶNG IM"),
+  new VideoVimeo(1, "189274572_th.jpg", "189274572", "Như Phút Ban Đầu - Noo Phước Thịnh", "Vì em anh như người điên mất trí. Vì em anh như chẳng còn biết nghĩ suy. Vì anh đã trót lỡ đắm say em không bận tâm mai sau thế nào..."),
+  new VideoVimeo(2, "189274612_th.jpg", "189274612", "Chúng ta không thuộc về nhau - Sơn Tùng MTP", "Niềm tin đã mất, giọt nước mắt cuốn kí ức anh chìm sâu. Tìm về nơi đâu, cô đơn đôi chân lạc trôi giữa bầu trời..."),
+  new VideoVimeo(3, "189275112_th.jpg", "189275112", "Gửi Anh Xa Nhớ - Bích Phương", "Anh xa nhớ anh có khỏe không. Em lâu lắm không viết thư tay..."),
+  new VideoVimeo(4, "189274543_th.jpg", "189274543", "We Don't Talk Anymore-Charlie-Puth vs Selena Gomez", "We Don't Talk Anymore, We Don't Talk Anymore..."),
+  new VideoVimeo(5, "189274526_th.jpg", "189274526", "Talk Love - K.Will", " Descendants Of The Sun OST "),
+  new VideoVimeo(6, "189274075_th.jpg", "189274075", "I Do - 911 ", "My whole world changed from the moment I met you. And it would never be the same..."),
+  new VideoVimeo(7, "189274027_th.jpg", "189274027", "Mashup-12 bài hot", " Lynkee"),
+  new VideoVimeo(8, "189274055_th.jpg", "189274055", "Thương - Karik ft Uyên Pím", "Lạ, yêu, hi sinh rồi thương. Tình cảm luôn dẫn dắt ta đến những cảm xúc vô thường"),
+  new VideoVimeo(9, "189274000_th.jpg", "189274000", "ERIK from ST.319 - Sau Tất Cả", "Sau tất cả, mình lại trở về với nhau. Tựa như chưa bắt đầu, tựa như ta vừa mới quen..."),
+  new VideoVimeo(10, "189273969_th.jpg", "189273969", "Yêu Một Người Có Lẽ - Lou Hoàng - Miu Lê ", "Giả vờ như tất cả mọi thứ chưa xảy ra với anh. Lập lờ những thứ ngay trước mắt anh chẳng hề so sánh..."),
+  new VideoVimeo(11, "189275094_th.jpg", "189275094", "Con Đường Xưa Em Đi - Ngô Trung Quang", "Con đường xưa em đi, vàng lên mái tóc thề, ngỏ buồn dâng tái tê....")
 ];
 
 function VideoVimeo(id, h, i, t, m){
